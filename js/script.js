a5efddc1c406f947d902149fcb3943e4945cbd5c
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-08-28 15:00:03 -0300

    concertando erro de javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 async function getAdvice() {
     const resposta = await fetch("https://api.adviceslip.com/advice");
     const data = await resposta.json();
-   // console.log(data)
-   // console.log(data.slip.advice)
     document.getElementById('advice').innerHTML = data.slip.advice;
 }
-let botaoAdvice = document.getElementById("get-advice")
-botaoAdvice.addEventListener("click", function{
+
+let botaoAdvice = document.getElementById("get-advice");
+botaoAdvice.addEventListener("click", function() { // Adicione os parênteses após "function"
     getAdvice();
-})
+});
